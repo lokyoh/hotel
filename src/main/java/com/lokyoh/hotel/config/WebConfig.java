e885aedec066472613f485a5133651fca3e834f7
l_o_o_k <2366360924@qq.com>
2024-12-26 12:44:34 +0800

    12.25

--- src/main/java/com/lokyoh/hotel/config/WebConfig.java
+++ src/main/java/com/lokyoh/hotel/config/WebConfig.java
@@ -5,6 +5,7 @@ import com.lokyoh.hotel.interceptors.LoginInterceptor;
 import com.lokyoh.hotel.interceptors.WorkerInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -22,5 +23,12 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(loginInterceptor).excludePathPatterns("/worker/login", "/user/login", "/user/register");
         registry.addInterceptor(workerInterceptor).addPathPatterns("/worker/**").excludePathPatterns("/worker/login");
         registry.addInterceptor(adminInterceptor).addPathPatterns("/admin/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT");
     }
 }
